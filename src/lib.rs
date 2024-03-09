5590f997165e0c5892c0782d41d1921003c6a55e
pichst123 <hoangpichgoodkid@gmail.com>
2024-03-10 00:53:21 +0700

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, near_bindgen, AccountId, BorshStorageKey};
+use near_sdk::{env, near_bindgen, AccountId, Timestamp};
 use near_sdk::json_types::U128;
 use near_sdk::serde::{Deserialize, Serialize};
-
+use near_sdk::serde_json::from_str;
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct DonationProject {
     projects: Vec<(String, ProjectMetadata)>, // Using a Vec to store project IDs paired with their metadata
     donations: Vec<(String, Vec<Donation>)>, // Using a Vec to store project IDs paired with a Vec of donations
+    project_counter: u64,
+
 }
 
 #[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, Clone)]
@@ -20,9 +22,10 @@ pub struct ProjectMetadata {
     current_amount: U128,
     ipfs_image: String,
     ipfs_hash: Vec<String>,
-    start_date: u64,
-    end_date: u64,
+    start_date: Timestamp,
+    end_date: Timestamp,
 }
+
 
 #[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, Clone)]
 #[serde(crate = "near_sdk::serde")]
@@ -32,6 +35,13 @@ pub struct Donation {
     donation_time: u64,
 }
 
+impl Default for DonationProject {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+
 #[near_bindgen]
 impl DonationProject {
     #[init]
@@ -40,11 +50,15 @@ impl DonationProject {
         Self {
             projects: Vec::new(),
             donations: Vec::new(),
+            project_counter: 0,
         }
     }
-
-    pub fn create_project(&mut self, project_id: String, creator_id: AccountId, project_name: String, project_description: String, target_amount: U128, ipfs_image: String, ipfs_hash: Vec<String>, end_date: u64) {
+    pub fn create_project(&mut self, project_name: String, project_description: String, target_amount: String, ipfs_image: String, ipfs_hash: Vec<String>, end_date: Timestamp) {        
+        let target_amount: U128 = from_str(&target_amount).expect("Invalid target amount");
+        let creator_id = env::signer_account_id();
         let start_date = env::block_timestamp();
+        self.project_counter += 1;
+        let project_id = format!("{}_{}_{}", creator_id, start_date, self.project_counter);
         let project_metadata = ProjectMetadata {
             creator_id,
             project_name,
@@ -60,8 +74,10 @@ impl DonationProject {
         assert!(self.projects.iter().all(|(id, _)| id != &project_id), "Project ID already exists");
         self.projects.push((project_id, project_metadata));
     }
+    
 
-    pub fn create_donation(&mut self, project_id: String, donor_id: AccountId, amount: U128) {
+    pub fn create_donation(&mut self, project_id: String, amount: U128) {
+        let donor_id = env::signer_account_id();
         let project_index = self.projects.iter().position(|(id, _)| id == &project_id).expect("Project not found");
         let (_, project_metadata) = &mut self.projects[project_index];
         project_metadata.current_amount = U128(project_metadata.current_amount.0 + amount.0);
